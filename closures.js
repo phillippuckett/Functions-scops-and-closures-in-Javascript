7c439bb4a2625d711dc2f63c683599a02b48ccf2
Phillip Puckett <phillippuckett88@Phillips-MacBook-Air.local>
2015-10-17 02:58:27 -0600

    first commit

--- closures.js
+++ closures.js
@@ -10,12 +10,13 @@ var outer = function(){
 //Invoke outer saving the return value into another variable called 'inner'.
 
   //Code Here
+var inner = outer();
 
 //Once you do that, invoke inner.
 
   //Code Here
 
-
+inner();
 
 //Next problem
 
@@ -34,7 +35,8 @@ var callFriend = function(){
 
   //Code Here
 
-
+var jakesCell = callFriend();
+jakesCell('435-215-9248');
 
 //Next Problem
 
@@ -45,6 +47,12 @@ var callFriend = function(){
 */
 
   //Code Here
+var makeCounter = function() {
+    var num = 1;
+    return function myCount() {
+        return (num++); 
+    }
+}
   var count = makeCounter();
   count() // 1
   count() // 2
@@ -58,11 +66,29 @@ var callFriend = function(){
 
 
 /*
-  Write a function that does something simple (console, alert, etc). Write a second function that accepts the first function as it's first parameter. The second function should return a new third function which, when invoked, invokes the first, original function that was passed in, but will only ever do so once.
+  Write a function that does something simple (console, alert, etc). 
+  Write a second function that accepts the first function as it's first parameter. 
+  The second function should return a new third function which, when invoked, invokes the first, 
+  original function that was passed in, but will only ever do so once.
 */
 
   //Code Here
 
+var simple = function() {
+    alert("suck chode!");
+}
+var simpler = function(par1) {
+    var i = 0;
+    return function() {
+        if (i < 1) {
+            i++;
+        par1();
+        }
+    }
+}
+
+var simplest = simpler(simple);
+simplest();
 
 
 //Next Problem
@@ -70,41 +96,68 @@ var callFriend = function(){
 
 
 /*
-  Now, similar to the last problem, write a function called 'fnCounter' that accepts two parameters. The first parameter will be an anonymous function and the second parameter, 'N', will be a number. Now, in 'fnCounter', allow the anonymous funciton to be invoked 'N' number of times. After it's been invoked 'N' number of times, return 'STOP'.
+  Now, similar to the last problem, write a function called 'fnCounter' that accepts two parameters. 
+  The first parameter will be an anonymous function and the second parameter, 'N', will be a number. 
+  Now, in 'fnCounter', allow the anonymous funciton to be invoked 'N' number of times. 
+  After it's been invoked 'N' number of times, return 'STOP'.
 */
 
+var fnCounter = function(callBack, N) {
+    N = N - 1;
+    callBack();
+    if (N > 0) {
+        fnCounter(callBack, N);
+    }
+    else {
+        return 'Stop';
+    }
+}
 
-
+var test = function() {
+    console.log("S a D");
+}
 //Next Problem
 
 
 
-/*
+
   var counter = function(){
-    for (var i=1; i<=5; i++) {
+    for (var i = 1; i <= 5; i++) {
       setTimeout( function timer(){
-          console.log( i );
-      }, i*1000 );
+          console.log(i);
+      }, i * 1000 );
     }
   };
+  counter();
+  
+  /*
 
   Above you have a function named counter. Examine the function (without running the code) then below write what you expect to happen when the funciton is invoked. *Hint: setTimeout calls a function or evaluates an expression after a specified number of milliseconds.
 
     //Answer Here
-
-
+Counter will console.log "1" after one second, "2" after two seconds and so on, until it hits five seconds
   Now, run the function in your console and note what happpens.
 
   Was your answer right or wrong?
 
     //Answer Here
-
+Not quite. The function counted along with the seconds
 
   Fix the counter function so that it works the way you expect it to work. (logging 1 then 2 then 3, etc)
 */
-
-    //Code Here
-
+// stack overflow for this --> http://stackoverflow.com/questions/20384719/why-does-javascript-settimeout-not-work-in-a-loop
+    
+//Code Here
+var counter = function(){
+    for (var i = 1; i <= 5; i++) {
+      setTimeout( (function(i) { 
+          return function timer() {
+            console.log(i);
+          }
+      })(i), i * 1000 );
+    }
+  };
+  counter();
 
 
 //Next Problem
@@ -123,5 +176,13 @@ var callFriend = function(){
 
   *Hint: Don't let this fool you. Break down what's really happening here.
 */
+var funcArray = [
+    function() {console.log(0)},
+    function() {console.log(1)},
+    function() {console.log(2)},
+    function() {console.log(3)},
+    function() {console.log(4)},
+    function() {console.log(5)},
+];
 
 
